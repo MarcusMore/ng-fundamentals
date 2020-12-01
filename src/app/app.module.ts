18068a7c4c309421f8f3aab6377fd046607037ce
Marcus More <marcusmore42@gmail.com>
2020-12-01 18:08:21 -0300

    fixed bootstrap issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from '../events/events-list.component'
 import { EventThumbnailComponent } from '../events/event-thumbnail.component'
+import { NavBarComponent } from '../nav/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [EventsAppComponent]
